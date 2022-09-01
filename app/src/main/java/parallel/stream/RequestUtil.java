1fc58c18f46f7a242a1b07f6c804d874db74ed5b
Derek Scharton <dscharton@hq.bill.com>
2022-09-01 09:29:40 -0700

    Parameterize request and update test

--- app/src/main/java/parallel/stream/RequestUtil.java
+++ app/src/main/java/parallel/stream/RequestUtil.java
@@ -1,15 +1,26 @@
 package parallel.stream;
 
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
-public class Request {
-    public String getData() {
+import java.io.IOException;
+import java.util.Optional;
+
+public class RequestUtil {
+    public String getData(Optional<Integer> delaySeconds, Optional<Integer> statusCode) {
+        Integer status = statusCode.orElse(200);
+        Integer delay = delaySeconds.orElse(0) ;
+        String url = String.format("https://httpstat.us/%s?sleep=%s", status, delay * 1000);
         OkHttpClient client = new OkHttpClient();
-        okhttp3.Request.Builder builder = new okhttp3.Request.Builder();
-        okhttp3.Request request = builder.url("https://httpstat.us")
+        Request.Builder builder = new Request.Builder();
+        Request request = builder.url(url)
                 .build();
-
-        return "";
+        try(Response response = client.newCall(request).execute()) {
+            return response.body().string();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
--- app/src/test/java/parallel/stream/RequestUtilTest.java
+++ app/src/test/java/parallel/stream/RequestUtilTest.java
@@ -1,2 +1,61 @@
-package parallel.stream;public class RequestUtilTest {
+package parallel.stream;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class RequestUtilTest {
+
+  @Test
+  @Disabled
+  void basic() {
+    RequestUtil util = new RequestUtil();
+    String data = util.getData(Optional.of(1000), Optional.of(200));
+    System.out.println(data);
+  }
+
+  @Test
+  void multi() {
+    List<String> l = new ArrayList<String>();
+    l.add("2, 200");
+    l.add("2, 200");
+    l.add("2, 200");
+    single(l);
+    para(l);
+  }
+
+  private void single(List<String> l) {
+    long start = Instant.now().toEpochMilli();
+    l.stream()
+        .forEach(
+            item -> {
+              int delay = Integer.parseInt(item.split(",")[0].trim());
+              int http = Integer.parseInt(item.split(",")[1].trim());
+              RequestUtil util = new RequestUtil();
+              String data = util.getData(Optional.of(delay), Optional.of(http));
+              System.out.println(data);
+            });
+    long finish = Instant.now().toEpochMilli();
+    System.out.println("single ==> " + (finish - start));
+  }
+
+  private void para(List<String> l) {
+
+    long start = Instant.now().toEpochMilli();
+    l.parallelStream()
+        .forEach(
+            item -> {
+              int delay = Integer.parseInt(item.split(",")[0].trim());
+              int http = Integer.parseInt(item.split(",")[1].trim());
+              RequestUtil util = new RequestUtil();
+              String data = util.getData(Optional.of(delay), Optional.of(http));
+              System.out.println(data);
+            });
+    long finish = Instant.now().toEpochMilli();
+    System.out.println("parallel ==> " + (finish - start));
+  }
 }
